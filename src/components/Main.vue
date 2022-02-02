58af98bbcb2fa20e72d0aa7563b855f9af8c0cb9
Masadavide <davide.masadm@gmail.com>
2022-02-02 15:21:38 +0100

    init

---
+++ src/components/Main.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <input type="text">
+    <button type="button">Cerca</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  name: 'Main',
+  props: {
+    
+  },
+  data() {
+      return {
+          
+      }
+  },
+  created(){
+      this.getBrani();
+  },
+  methods: {
+      cercaFilm: function(){
+        axios
+          .get('https://developers.themoviedb.org/3/search/movies', {
+            params: {
+              api_key: '52964418bdf199e6c4cf74a2f4a59238',
+              query: '',
+            }
+          })
+      }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
